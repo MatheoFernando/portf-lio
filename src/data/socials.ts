860214405ca4ed8de6e56395050d75c5448a2473
jvieirafernando20@gmail.com <jvieirafernando20@gmail>
2025-01-21 11:47:24 +0100

    almost done, falta os projectos

---
+++ src/data/socials.ts
@@ -0,0 +1,16 @@
+import IconGithub from "../icons/github.astro";
+import IconLinkedin from "../icons/linkedin.astro";
+
+export const SOCIALS = [
+
+  {
+    name: "Github",
+    url: "https://github.com/MatheoFernando",
+    icon: IconGithub,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/vieira-fernando-2b66ab331/",
+    icon: IconLinkedin,
+  },
+];
